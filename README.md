a5f27be737dcb3a027917e1fcc952eb5af857990
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-10-23 18:28:47 -0500

    commit changes

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-Zillow project information
+Zillow Project Information
+
+Important:
+
+1. Produce a model that is configured to predict the values of single unit properties that the tax district assesses using the property data from those whose last transaction was during the "hot months" (in terms of real estate demand) of May and June in 2017.
+
+2. Provide the distribution of tax rates by county. Include the distribution of tax rates for each county inorder to see how much they vary within the properties in the county and the rates the bulk of the properties sit around.
+
+3. For the first iteration of the Zestimate model, use only square feet of the home, number of bedrooms, and number of bathrooms to estimate the properties assessed value. The result will be the mvp (minimally viable product).
+
+Pipeline and special instructions:
+
+ACQIUIRE - Obtained data from SQL zillow database. Only included "single family residences" with a vaild "bedroom count" and "bathroom count"
+
+
+PREP - 
+
+
+SPLIT & SCALE - 
+
+
+DATA EXPLORATION - 
+
+
+MODELING & EVALUATION - 
+
--- acquire.py
+++ acquire.py
@@ -1,43 +1,42 @@
 import pandas as pd
 import numpy as np
-
 import env
 
 def get_db_url(database_name):
     return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{database_name}'
 
-def get_data_from_mysql():
+def get_zillow_data():
     query = '''
-    SELECT customer_id, monthly_charges, tenure, total_charges
-    FROM customers WHERE contract_type_id = 3
-    ORDER BY total_charges DESC;
+    SELECT bedroomcnt as bedroom_count, 
+    bathroomcnt as bathroom_count, 
+    calculatedfinishedsquarefeet as square_feet,
+    taxvaluedollarcnt as property_value,
+    taxamount as tax_paid,
+    (taxamount / taxvaluedollarcnt) * 100 as tax_rate,
+    proptype.propertylandusedesc as property_type, 
+    fips as county_code
+    FROM properties_2017 AS property
+    JOIN predictions_2017 AS pred USING(parcelid)
+    JOIN propertylandusetype AS proptype USING(propertylandusetypeid)
+    WHERE (transactiondate >= '2017-05-01' AND transactiondate <= '2017-06-30')
+    AND (propertylandusedesc = 'Single Family Residential')
+    AND property.bedroomcnt > 0
+    AND property.bathroomcnt > 0
+    AND property.taxvaluedollarcnt > 0
+    AND property.lotsizesquarefeet > 0
+    ORDER BY fips;
     '''
 
-    df = pd.read_sql(query, get_db_url('telco_churn'))
-    return df
+    df = pd.read_sql(query, get_db_url('zillow'))
+    return df    
+
+df = get_zillow_data()
 
 def clean_data(df):
-    df = df[df.total_charges != ' ']
-    df.total_charges = df.total_charges.astype(float)
+    df = df.dropna()
     return df
 
-def wrangle_telco():
-    df = get_data_from_mysql()
+def wrangle_zillow():
+    df = get_zillow_data()
     df = clean_data(df)
-    return df
-
-def get_titanic_data():
-    query = '''
-    SELECT * FROM passengers
-    '''
-
-    df = pd.read_sql(query, get_db_url('titanic_db'))
-    return df
-
-def get_iris_data():
-    query = '''
-    SELECT * FROM measurements 
-    JOIN species USING(species_id)
-    '''
-    df = pd.read_sql(query,get_db_url('iris_db'))
     return df
